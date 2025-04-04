e82af962b7645656467c6736ee65f39f8269b387
aadhaar-debug <aadhaarkoul2002@gmail.com>
2025-04-05 00:33:52 +0530

    Hid the navops

--- src/App.js
+++ src/App.js
@@ -21,17 +21,20 @@ const App = () => {
         <div className="navops">About</div>
         <div className="navops">Blog</div>
         <div className="navops">Contact</div>
-        
-
-        <div class="search-container">
-            <input type="text" placeholder="Search..."></input>
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                  <circle cx="11" cy="11" r="8"></circle>
-                  <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-              </svg>
-          </button>
+        <div className="navops">
+          <div class="search-container">
+              <input type="text" placeholder="Search...">
+                </input>
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                      <circle cx="11" cy="11" r="8"></circle>
+                      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                  </svg>
+                </button>
+          </div>
         </div>
+
+        
       </div>
 
 
