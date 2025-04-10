e831693ecdee5b85a6f4bba05140ed10c9946de1
aadhaar-debug <aadhaarkoul2002@gmail.com>
2025-04-10 18:46:57 +0530

    made the template

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,14 @@ import banner from "../assets/images/kailashbanner.png";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="banner">
-        <img src={banner} alt="Banner" />
-      </div>
-
+    <div
+      className="home-container"
+      style={{ backgroundImage: `url(${banner})` }}
+    >
       <div className="home-content">
-        {/* Your Home Page Content */}
+        <h1>Welcome to Kailash Vidyadham</h1>
+        <p>Spiritual. Peaceful. Divine. Sacred. Eternal.</p>
+        <button className="banner-button">Contact Us</button>
       </div>
     </div>
   );
