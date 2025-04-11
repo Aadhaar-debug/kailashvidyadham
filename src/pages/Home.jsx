032a5ff7492e20573eb25aab01a7d0105a3e56ad
aadhaar-debug <aadhaarkoul2002@gmail.com>
2025-04-11 18:07:43 +0530

    Added the online form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -98,7 +98,7 @@ const Home = () => {
             <div className="row justify-content-center" style={{ rowGap: '10%' }}>
 
               <div className="col-md-2 mb-4">
-                <div className="card">
+                <div className="card" >
                   <img className="card-img-top" src={accomodation} alt="Card image cap" />
                   <div className="card-body">
                     <h5 className="card-title">Card Title 1</h5>
