6bdf6a8278496f79dc533986b26cf1ae5f0e45cc
aadhaar-debug <aadhaarkoul2002@gmail.com>
2025-04-12 21:05:37 +0530

    fixed the online form dimensions

--- src/components/movetotop.jsx
+++ src/components/movetotop.jsx
@@ -41,12 +41,13 @@ const MoveToTop = () => {
                         borderRadius: "50%",
                         width: "50px",
                         height: "50px",
-                        fontSize: "20px",
+                        fontWeight:"900",
+                        fontSize: "36px",
                         cursor: "pointer",
                         boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
                     }}
                 >
-                    ↑
+                    <div>^</div>
                 </button>
             )}
         </div>
