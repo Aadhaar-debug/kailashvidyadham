a0d6b062c2909fbd1a882a2ef08d751018ce9332
aadhaar-debug <aadhaarkoul2002@gmail.com>
2025-04-13 13:20:46 +0530

    FInished services page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,23 +5,7 @@ const Contact = () => {
   return (
     <div className="mothercontainer" style={{marginBottom : '-3vh' ,backgroundColor: 'rgb(191,0,0)', color: 'white', padding: '20px', textAlign: 'center'}}>
       <h1 style={{color:'gold' , marginTop:'5vh' , marginBottom:'5vh'}}>Contact Us</h1>
-      <p>Phone: +91-9419362813</p>
-      <p>Email - akhandbharat@gmail.com</p>
-      <p>Address: QRHQ+3JG , Upper Chinore , Jammu, Jammu and Kashmir , 180013</p>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '5vh', marginTop: '10px' , marginBottom:'5vh'}}> 
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" style={{ width: '30px', height: '30px' }} />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" style={{ width: '30px', height: '30px' }} />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" style={{ width: '30px', height: '30px' }} />
-        </a>
-        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" alt="YouTube" style={{ width: '30px', height: '30px' }} />
-        </a>
-      </div>
+      
       <div className="contactcontain">
         <div className="contactphoto">
           <img src={photo22} alt="Contact" />
@@ -45,6 +29,30 @@ const Contact = () => {
           </form>
         </div>
       </div>
+
+
+      <div className='contactothertext'>
+        <p>Phone: +91-9419362813</p>
+        <p>Email - akhandbharat@gmail.com</p>
+        <p>Address: QRHQ+3JG , Upper Chinore , Jammu, Jammu and Kashmir , 180013</p>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '5vh', marginTop: '10px' , marginBottom:'5vh'}}> 
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" style={{ width: '30px', height: '30px' }} />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" style={{ width: '30px', height: '30px' }} />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" style={{ width: '30px', height: '30px' }} />
+          </a>
+          <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" alt="YouTube" style={{ width: '30px', height: '30px' }} />
+          </a>
+        </div>
+      </div>
+
+
+
     </div>
   )
 }
